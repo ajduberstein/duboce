194cd28b27d12cb107a95165cc559daeeb54c539
Andrew Duberstein <ajduberstein@gmail.com>
2022-06-05 10:34:50 -0700

    Fix test

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,10 +1,10 @@
 from duber import Dot
 
 fixture = [
-    {'name': 'John', 'height': 178.8},
-    {'name': 'Paul', 'height': 180}
-    {'name': 'George', 'height': 175},
-    {'name': 'Ringo', 'height': 173},
+    {'name': 'John', 'height': 178.8, 'instrument': 'guitar'},
+    {'name': 'Paul', 'height': 180, 'instrument': 'bass'}
+    {'name': 'George', 'height': 175, 'instrument': 'guitar'},
+    {'name': 'Ringo', 'height': 173, 'instrument': 'drums'},
 ]
 
 def test_basic_json():
